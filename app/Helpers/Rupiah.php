b2ae4f4b16acfb429a4c843db439f8540144da5e
rafianramadhani@gmail.com <rafianramadhani@gmail.com>
2021-07-22 08:25:05 +0700

    delete file rusak

--- app/Helpers/Rupiah.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Helpers;
-
-class Rupiah {
-    public static function get_rupiah($nominal) {
-        return "Rp ".number_format($nominal, 0, ',', '.');
-    }
-}
--- resources/views/alert/success.blade.php
+++
@@ -1,6 +0,0 @@
-@if(session('status'))
-<div class="alert alert-success alert-block">
-  <button type="button" class="close" data-dismiss="alert">×</button>
-    <strong>{{session('status')}}</strong>
-</div>
-@endif
